0947359c34da592e902bec0bc83f5a2ed8a5f4f2
Sam Rolfe <srolfe01@comp112-12.eecs.tufts.edu>
2024-10-11 11:51:25 -0400

    Server successfully listens

---
+++ client/client.c
@@ -0,0 +1,28 @@
+//----HEADER---------------------------------------------------------------------------------------
+// Author:      Sam Rolfe
+// Date:        October 2024
+// Script:      client.c
+// Usage:       Implementation file for client
+//*************************************************************************************************
+#include "client.h"
+#include <string.h>
+// #include <math.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/select.h> // Provides select() functionality
+// #include <netinet/in.h> // Provides sockaddr_in struct
+
+
+// ----GLOBAL VARIABLES----------------------------------------------------------------------------
+
+
+//----FUNCTIONS------------------------------------------------------------------------------------
+
+void connect_to_server(int LISTENING_PORT){
+    1+1;
+    return NULL;
+}
+
+
+
+//-------------------------------------------------------------------------------------------------
--- server/server.c
+++ server/server.c
@@ -6,15 +6,20 @@
 //*************************************************************************************************
 #include "server.h"
 #include <string.h>
-// #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>  // For socket        
+#include <sys/socket.h> 
+#include <netinet/in.h> // Provides sockaddr_in struct
+#include <arpa/inet.h>
+#include <netdb.h>      // Provides hostent struct
 #include <sys/select.h> // Provides select() functionality
 #include <netinet/in.h> // Provides sockaddr_in struct
 
 
 // ----GLOBAL VARIABLES----------------------------------------------------------------------------
-#define BUFFER_MAX_SIZE 512  // Confirm size
+#define BUFFER_MAX_SIZE 512       // Confirm size
+#define MAX_CLIENT_CONNECTIONS 10
 
 //----FUNCTIONS------------------------------------------------------------------------------------
 // Max function (since max isn't in stdlib)
@@ -22,11 +27,34 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-void initialize_server(int LISTENING_SOCKET) {
-    int client_socket;
-    struct sockaddr_in client_addr;
+int initialize_server(int PORT) {
+    int client_socket, listening_socket;
+    struct sockaddr_in client_addr, server_addr;
     socklen_t client_addr_size = sizeof(client_addr);
     char* buffer = malloc((size_t) BUFFER_MAX_SIZE);
+
+    // Create listening socket
+    listening_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(listening_socket < 0) {
+        perror("Error creating listening socket");
+        return -1;
+    }
+
+    // Initialize fields of struct for server address
+    memset(&server_addr, 0, sizeof(server_addr)); // Set structure to 0's, ensuring sin_zero is all zeros
+    server_addr.sin_family = AF_INET;             // Set address family to IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY;     // Set IP address to all IP addresses of machine
+    server_addr.sin_port = htons(PORT);           // Set port number
+
+    // Bind socket to IP address and port
+    if(bind(listening_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0 ){
+        perror("Error binding socket");
+        return -1;
+    }
+
+    // Listen for incoming connections requests on "listening socket"
+    listen(listening_socket, MAX_CLIENT_CONNECTIONS);
+    printf("Listening for incoming connection requests on port %d...\n\n", PORT);
 
     // Create file descriptor sets needed for select()
     fd_set master_set; 
@@ -37,35 +65,43 @@ void initialize_server(int LISTENING_SOCKET) {
     FD_ZERO(&temp_set);
 
     // Add listening socket to master set and set to max file descriptor
-    FD_SET(LISTENING_SOCKET, &master_set);
-    int fdmax = LISTENING_SOCKET;
+    FD_SET(listening_socket, &master_set);
+    int fdmax = listening_socket;
 
-    // while(1){
-    //     // Copy master set to temp set (to preserve master
-    //     // set while select() modifies copy)
-    //     temp_set = master_set;
+    while(1){
+        // Copy master set to temp set (to preserve master
+        // set while select() modifies copy)
+        temp_set = master_set;
 
-    //     // Block until a socket is ready
-    //     select(fdmax+1, &temp_set, NULL, NULL, NULL);
+        // Block until a socket is ready
+        select(fdmax+1, &temp_set, NULL, NULL, NULL);
 
-    //     // If master socket is ready, a new client has requested
-    //     // connection. Accept new client and add to master set
-    //     if(FD_ISSET(LISTENING_SOCKET, &temp_set)){
-    //         printf("A new client wants to connect\n");
-    //         client_socket = accept(LISTENING_SOCKET, (struct sockaddr *) &client_addr, &client_addr_size);
+        // If master socket is ready, a new client has requested
+        // connection. Accept new client and add to master set
+        if(FD_ISSET(listening_socket, &temp_set)){
+            client_socket = accept(listening_socket, (struct sockaddr *) &client_addr, &client_addr_size);
+            printf("New client connection. Server socket: %d, IP: %s, Port: %d\n", 
+                    client_socket, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-    //         // Add new client to master set and update max file descriptor
-    //         FD_SET(client_socket, &master_set);
-    //         fdmax = max(client_socket, fdmax);
-    //     } else {
-    //         for(int socket = 0; socket <= fdmax; socket++) {
-    //             if(FD_ISSET(socket, &temp_set)) {
-    //                 int bytes_received = recv(socket, buffer, (size_t) BUFFER_MAX_SIZE-1, 0);
-    //             }
-    //         }
-    //     }
-    // }
+            // Add new client to master set and update max file descriptor
+            FD_SET(client_socket, &master_set);
+            fdmax = max(client_socket, fdmax);
+        } else {
+            for(int socket = 0; socket <= fdmax; socket++) {
+                if(FD_ISSET(socket, &temp_set)) {
+                    printf("Client is sending message\n");
+                    // int bytes_received = recv(socket, buffer, (size_t) BUFFER_MAX_SIZE-1, 0);
+                }
+            }
+        }
+    }
+
+    // Free data and close sockets
     free(buffer);
+    for(int socket = 0; socket <= fdmax; socket++) {
+        close(socket);
+    }
+    return 0;
 }
 
 
